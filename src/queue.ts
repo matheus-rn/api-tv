db3f73f69c8024003f31316a4ba40fe21902070d
matheus-rn <matheus.1nascimento58@gmail.com>
2020-02-22 22:22:25 -0300

    Adicionando cron

--- src/queue.ts
+++ src/queue.ts
@@ -1,15 +1,18 @@
-import ScrapyTv from './app/jobs/ScrapyTv'
+// import ScrapyTv from './app/jobs/ScrapyTv'
 import Queue from './app/lib/Queue'
 
 const url = process.env.API_SCRAPY
 const categories = process.env.CATEGORIES.split(',')
 
-categories.forEach(categorie => {
-  const data = {
-    url: `${url}/${categorie}`
-  }
-  Queue.add(data)
-})
+// categories.forEach(categorie => {
+//   const data = {
+//     url: `${url}/${categorie}`
+//   }
+
+const data = {
+  url: 'url'
+}
+// })
 
 // Queue.process(async job => {
 //   await ScrapyTv.handle(job.data.url)
@@ -18,3 +21,5 @@ categories.forEach(categorie => {
 Queue.process(async job => {
   console.log(job.data.url)
 })
+
+Queue.add(data, { repeat: { cron: '0 0/1 * * * *' } })

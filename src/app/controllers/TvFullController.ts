969980400ae0e707dfdf5939c39a390a1150c40f
matheus-rn <matheus.1nascimento58@gmail.com>
2020-02-29 13:18:15 -0300

    Finalizado função que retorna o programa atual e o proximo programa

--- src/app/controllers/TvFullController.ts
+++ src/app/controllers/TvFullController.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from 'express'
 import esClient from '../../config/elasticsearch'
+import { zonedTimeToUtc } from 'date-fns-tz'
+import { subHours } from 'date-fns'
 
 class TvFullController {
   public async searchCurrentPrograms (req: Request, res: Response): Promise<Response> {
-    const programs = []
     const paramsQuery = {
       index: 'channels',
       type: 'doc',
@@ -13,14 +14,28 @@ class TvFullController {
             channel: 'Canal Brasil'
           }
         },
-        sort: [{ datetime: 'asc' }]
+        sort: [{ datetime: 'asc' }],
+        size: 1000
       }
     }
 
     let documents = (await esClient.search(paramsQuery)).body.hits.hits
     documents = documents.map(document => document._source)
-    const indexProgram = documents.findIndex(document => document.datetime === '2020-02-29T04:00:00')
-    return res.json(indexProgram)
+
+    const timeZone = zonedTimeToUtc(subHours(new Date(), 3), 'America/Sao_Paulo')
+
+    const indexProgram = documents.findIndex(document => {
+      const timeZoneProgram = zonedTimeToUtc(subHours(new Date(document.datetime), 3), 'America/Sao_Paulo')
+
+      return timeZoneProgram.getTime() > timeZone.getTime()
+    })
+
+    const programs = {
+      currentProgram: documents[indexProgram - 1],
+      nextProgram: documents[indexProgram]
+    }
+
+    return res.json(programs)
   }
 }
 

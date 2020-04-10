a65a5879b75ecd9ff822fbe8dc1e7ad802956eb7
matheus-rn <matheus.1nascimento58@gmail.com>
2020-04-10 14:08:20 -0300

    Configurando puppeteer

--- src/app/controllers/TvFullController.ts
+++ src/app/controllers/TvFullController.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from 'express'
 import esClient from '../../config/elasticsearch'
 import { zonedTimeToUtc } from 'date-fns-tz'
-import { subHours } from 'date-fns'
 
 class TvFullController {
   public async searchCurrentPrograms (req: Request, res: Response): Promise<Response> {
